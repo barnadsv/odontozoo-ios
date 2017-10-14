51e9a634d99172ca8b1928c72c89ac31e72fbae6
Leonardo Barnabe <barnadsv@gmail.com>
2017-10-14 12:00:13 -0300

    OdontogramaImages com header

---
+++ odontozoo-ios/OdontogramaImagesHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  OdontogramaImagesHeaderViewControllerCollectionReusableView.swift
+//  odontozoo-ios
+//
+//  Created by Leonardo de Araujo Barnabe on 14/10/17.
+//  Copyright © 2017 Leonardo de Araujo Barnabe. All rights reserved.
+//
+
+import UIKit
+
+class OdontogramaImagesHeaderView: UICollectionReusableView {
+        
+    @IBOutlet weak var lblTitulo: UILabel!
+}
--- odontozoo-ios/OdontogramaImagesViewController.swift
+++ odontozoo-ios/OdontogramaImagesViewController.swift
@@ -11,13 +11,9 @@ import Firebase
 import FirebaseStorageUI
 import SDWebImage
 
-//private let reuseIdentifier = "OdontogramaImageCell"
-
 class OdontogramaImagesViewController: UICollectionViewController {
 
     var imagens: [Imagem] = []
-//    var imagesContent: [UIImage] = []
-//    var image: UIImage! = nil
     fileprivate let sectionInsets = UIEdgeInsets(top: 25.0, left: 25.0, bottom: 25.0, right: 25.0)
     let reuseIdentifier = "OdontogramaImageCell"
     fileprivate let imagem: Imagem! = nil
@@ -26,7 +22,6 @@ class OdontogramaImagesViewController: UICollectionViewController {
     var usuario: Usuario!
     var odontogramaId: String! = ""
     var imageWidth: CGFloat! = 200.0
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,6 +63,7 @@ class OdontogramaImagesViewController: UICollectionViewController {
     
     // Quando a view começa a ser carregada
     override func loadView() {
+        
         super.loadView()
         let detailTabController = self.tabBarController?.viewControllers?.first as! OdontogramaDetailViewController
         self.usuario = detailTabController.usuario
@@ -83,49 +79,6 @@ class OdontogramaImagesViewController: UICollectionViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
 
@@ -141,105 +94,40 @@ extension OdontogramaImagesViewController {
         return self.imagens.count
     }
     
-    //3
-//    override func collectionView(_ collectionView: UICollectionView,
-//                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
-//                                                      for: indexPath)
-//        cell.backgroundColor = UIColor.black
-//        // Configure the cell
-//        return cell
-//    }
-    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         //1
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.reuseIdentifier,
                                                       for: indexPath) as! OdontogramaImageCollectionViewCell
-        //2
-//        if let imagemOdontogramaUrl = imagens[indexPath.row].strUri {
-//            let url = URL(string: imagemOdontogramaUrl)
-//            URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
-//                if (error != nil) {
-//                    print(error)
-//                    return
-//                }
-//                
-//                DispatchQueue.main.async(execute: {
-//                    cell.backgroundColor = UIColor.white
-//                    cell.imageView?.image = UIImage(data: data!)
-//                })
-//                
-//            })
-//        }
         
         if let imagemOdontogramaUrl = imagens[indexPath.row].strUri {
             
             cell.imageView?.sd_setIndicatorStyle(.gray)
             cell.imageView?.sd_setShowActivityIndicatorView(true)
-            //cell.imageView?.sd_setImage(with: URL(string: imagemOdontogramaUrl), placeholderImage: UIImage(named: "placeholder.png"))
-            
-            
             let imageStorage = Storage.storage().reference(forURL: imagemOdontogramaUrl)
             cell.imageView?.sd_setImage(with: imageStorage, placeholderImage: UIImage(named: "placeholder.png"))
-            
-            
-//            let imageStorage = Storage.storage().reference(forURL: imagemOdontogramaUrl)
-//            imageStorage.getData(maxSize: INT64_MAX) { (data, error) in
-//                if let error = error {
-//                    print("Error downloading image data: \(error)")
-//                    return
-//                }
-//                cell.backgroundColor = UIColor.white
-//                cell.imageView?.image = UIImage(data: data!)
-//            }
+
         }
-        
-        
-//        storageRef.data(withMaxSize: INT64_MAX){ (data, error) in
-//            if let error = error {
-//                print("Error downloading image data: \(error)")
-//                return
-//            }
-//            
-//            // 3
-//            storageRef.metadata(completion: { (metadata, metadataErr) in
-//                if let error = metadataErr {
-//                    print("Error downloading metadata: \(error)")
-//                    return
-//                }
-//                
-//                // 4
-//                if (metadata?.contentType == "image/gif") {
-//                    mediaItem.image = UIImage.gifWithData(data!)
-//                } else {
-//                    mediaItem.image = UIImage.init(data: data!)
-//                }
-//                self.collectionView.reloadData()
-//                
-//                // 5
-//                guard key != nil else {
-//                    return
-//                }
-//                self.photoMessageMap.removeValue(forKey: key!)
-//            })
-//        }
-        
-        //cell.backgroundColor = UIColor.white
-        //3
-        //cell.imageView.image = imagemOdontograma.thumbnail
-        //cell.backgroundColor = UIColor.black
-        
-//        let image = UIImage(named: "bandeiras")
-//        print(image?.size)
-//        //let newImage = resizeImage(image: image!, newWidth: self.imageWidth)
-//        //print(newImage.size)
-//        //cell.imageView?.image = newImage
-//        cell.imageView?.image = UIImage(named: "bandeiras")
-//        print(cell.imageView?.image?.size)
-//        cell.imageView?.layer.borderWidth = 1.0
-//        cell.imageView?.layer.borderColor = UIColor.black.cgColor
-        
+
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView,
+                                 viewForSupplementaryElementOfKind kind: String,
+                                 at indexPath: IndexPath) -> UICollectionReusableView {
+        //1
+        switch kind {
+        //2
+        case UICollectionElementKindSectionHeader:
+            //3
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                             withReuseIdentifier: "OdontogramaImagesHeaderView",
+                                                                             for: indexPath) as! OdontogramaImagesHeaderView
+            headerView.lblTitulo.text = "Imagens do Odontograma"
+            return headerView
+        default:
+            //4
+            assert(false, "Unexpected element kind")
+        }
     }
     
 
@@ -251,29 +139,6 @@ extension OdontogramaImagesViewController : UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        
-//        if let imagemOdontogramaUrl = imagens[indexPath.row].strUri {
-//            let imageStorage = Storage.storage().reference(forURL: imagemOdontogramaUrl)
-//            imageStorage.getData(maxSize: INT64_MAX) { (data, error) in
-//                if let error = error {
-//                    print("Error downloading image data: \(error)")
-//                    return
-//                }
-//                //cell.backgroundColor = UIColor.white
-//                //cell.imageView?.image = UIImage(data: data!)
-//                self.image = UIImage(data: data!)
-//                self.imagesContent.append(self.image!)
-//                
-//                let ratio = self.image.size.width / self.image.size.height
-//                let paddingSpace = self.sectionInsets.left * (itemsPerRow + 1)
-//                let availableWidth = view.frame.width - paddingSpace
-//                let widthPerItem = availableWidth / self.itemsPerRow
-//                let heightPerItem = widthPerItem / ratio
-//                
-//                return CGSize(width: widthPerItem, height: heightPerItem)
-//            }
-//        }
         
         //2
         let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
@@ -297,6 +162,25 @@ extension OdontogramaImagesViewController : UICollectionViewDelegateFlowLayout {
                         minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return sectionInsets.left
     }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
+        switch UIDevice.current.orientation{
+            case .portrait:
+                return CGSize(width: UIScreen.main.bounds.width, height: 100)
+            case .portraitUpsideDown:
+                return CGSize(width: UIScreen.main.bounds.width, height: 100)
+            case .landscapeLeft:
+                return CGSize(width: UIScreen.main.bounds.width, height: 75)
+            case .landscapeRight:
+                return CGSize(width: UIScreen.main.bounds.width, height: 75)
+            default:
+                return CGSize(width: UIScreen.main.bounds.width, height: 100)
+        }
+        
+    }
+    
 }
 
 private extension OdontogramaImagesViewController {
